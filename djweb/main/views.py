2906f5d7298d3e995b9fb9e97214d44dfe0ff621
Denys Bokov <denisbokov1703@gmail.com>
2024-12-08 14:57:15 +0100

    feat: render pages with django html templates

--- djweb/main/views.py
+++ djweb/main/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h4>Hello Django</h4>')
+    return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse('<h4>About us</h4>')
+    return render(request, 'main/about.html')
