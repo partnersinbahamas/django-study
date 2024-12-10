33eb405882f70e1e3d16c7a6ade5580aafb8b32b
Denys Bokov <denisbokov1703@gmail.com>
2024-12-10 18:35:08 +0100

    feat: add data to each html template an render them

--- djweb/main/views.py
+++ djweb/main/views.py
@@ -3,7 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'buttons': ['Button-1', 'Button-2', 'Button-3'],
+        'buttonClass': 'btn-danger'
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
-    return render(request, 'main/about.html')
+    data = {
+      'text': 'This page will contain a certain amount of text that describes information about the web page.',
+      'buttons': ['Click me'],
+      'buttonClass': 'btn-warning'
+    }
+    return render(request, 'main/about.html', data)
