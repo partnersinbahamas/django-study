af8fb5284c00e7194aa0dfda3471128322fd20cd
Denys Bokov <denisbokov1703@gmail.com>
2024-12-10 19:27:57 +0100

    feat: create Article model in data base and make migrations

--- djweb/news/models.py
+++ djweb/news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Create your table models for Data Base here.
+
+# To create this model in DB you need to make migration
+# to create migrations file use => python manage.py makemigrations
+
+# To run migrations run python manage.py migrate
+class Article(models.Model):
+    title = models.CharField('Name', max_length=50)
+    desctiption = models.CharField('Desctiption', max_length=250)
+    text = models.TextField('State')
+    date = models.DateTimeField('Date')
+
+    def __self__(self):
+        return self.title
